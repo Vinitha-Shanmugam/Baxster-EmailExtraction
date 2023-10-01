e3feb990f754ab6bdb4db9bf9003bdd9b63f0f84
Vinitha-Shanmugam <vinitha.s@vrdella.com>
2023-10-01 15:10:58 +0530

    first commit

--- al.py
+++ al.py
@@ -1,72 +1,65 @@
 import imaplib
 import email
 import html2text
-import re
-from datetime import datetime
 
-user = 'vinitha.s@vrdella.com'
-password = 'udbb zntf hhwe fbxl'
 
-imap_url = 'imap.gmail.com'
+def Email(username, password):
+    imap_url = 'imap.gmail.com'
 
-# Connection with GMAIL using SSL
-my_mail = imaplib.IMAP4_SSL(imap_url)
+    # Connection with GMAIL using SSL
+    my_mail = imaplib.IMAP4_SSL(imap_url)
 
-# Log in using your credentials
-my_mail.login(user, password)
+    # Log in using your credentials
+    my_mail.login(username, password)
 
-my_mail.select('Alabama')
+    my_mail.select('Alabama')
 
-# key = 'FROM'
-# value = 'jobs_della@vrdella.com'
-_, data = my_mail.search(None, 'UNSEEN')
+    li = []
 
-mail_id_list = data[0].split()  # IDs of all emails that we want to fetch
+    # Iterate through unseen emails
+    _, data = my_mail.search(None, 'UNSEEN')
+    mail_id_list = data[0].split()  # IDs of all unseen emails
 
-li = []
-msgs = []  # empty list to capture all messages
-# Iterate through messages and extract data into the msgs list
-for num in mail_id_list:
-    typ, data = my_mail.fetch(num, '(RFC822)')  # RFC822 returns the whole message (BODY fetches just body)
-    msgs.append(data)
+    for num in mail_id_list:
+        typ, data = my_mail.fetch(num, '(RFC822)')
+        msg_data = data[0][1]  # Extract message data
+        li.append(msg_data)  # Append message data to the list
 
-# Create an instance of html2text
-html_converter = html2text.HTML2Text()
+    return li
 
-for msg in msgs[::-1]:
-    for response_part in msg:
-        if type(response_part) is tuple:
-            my_msg = email.message_from_bytes(response_part[1])
-            formatted_result = {}
-            # print("_________________________________________")
-            # print("subj:", my_msg['subject'])
-            # print("from:", my_msg['from'])
-            # print("body:")
-            if 'closed' in my_msg['subject']:
-                formatted_result['Status'] = "closed"
-            if 'Hold' in my_msg['subject']:
-                formatted_result['Status'] = 'Hold'
 
-            if "Requisition #" in my_msg['subject']:
-                value = my_msg['subject'].split("Requisition #")[1].strip('\n')
-                value = value.strip().split('\n')[0].split()[0]
-                formatted_result['Requisition ID'] = value
+def logic(msg_data):
+    # Initialize the result dictionary for this email
+    formatted_result = {}
+    html_converter = html2text.HTML2Text()
+    my_msg = email.message_from_bytes(msg_data)
 
-            for part in my_msg.walk():
-                # print(part.get_content_type())
-                if part.get_content_type() == 'text/plain':
-                    plain_text = part.get_payload()
-                elif part.get_content_type() == 'text/html':
-                    # Convert HTML to plain text using html2text
-                    html_content = part.get_payload(decode=True).decode()
-                    plain_text = html_converter.handle(html_content)
-                    # match = re.search(r'Requisition\s*#?\s*:\s*(\d+)', plain_text)
-                    # if match:
-                    #         formatted_result["Requisition ID"] = match.group(1)
-                    if "The following" in plain_text:
-                        value = plain_text.split("The following")[1].strip('\n')
-                        value = value.strip().split('.')[0]
-                        formatted_result["comment"] = value
+    if 'closed' in my_msg['subject']:
+        formatted_result['Status'] = "closed"
+    if 'Hold' in my_msg['subject']:
+        formatted_result['Status'] = 'Hold'
 
-            li.append(formatted_result)
-print(li)
+    if "Requisition #" in my_msg['subject']:
+        value = my_msg['subject'].split("Requisition #")[1].strip('\n')
+        value = value.strip().split('\n')[0].split()[0]
+        formatted_result['Requisition ID'] = value
+
+    # Extract 'comment' from email body
+    for part in my_msg.walk():
+        if part.get_content_type() == 'text/plain':
+            plain_text = part.get_payload()
+        if part.get_content_type() == 'text/html':
+            html_content = part.get_payload(decode=True).decode()
+            plain_text = html_converter.handle(html_content)
+            if "The following" in plain_text:
+                value = plain_text.split("The following")[1]
+                value = value.split('.')[0]
+                formatted_result["comment"] = value
+
+    return formatted_result
+
+
+email_data_list = Email('vinitha.s@vrdella.com', 'udbb zntf hhwe fbxl')
+for msg_data in email_data_list:
+    result = logic(msg_data)
+    print(result)
