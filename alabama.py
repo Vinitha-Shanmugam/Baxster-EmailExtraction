80b40a51df2146132b6184d1db1879e354ed7967
Vinitha-Shanmugam <vinitha.s@vrdella.com>
2023-10-03 14:42:50 +0530

    added

--- alabama.py
+++ alabama.py
@@ -20,50 +20,48 @@ def Email(username, password):
     _, data = my_mail.search(None, 'UNSEEN')
     mail_id_list = data[0].split()  # IDs of all unseen emails
 
-
-
     # Iterate through unseen emails and extract data
     for num in mail_id_list:
-        typ, data = my_mail.fetch(num, '(RFC822)')
-        msg_data = data[0][1]  # Extract message data
-        li.append(msg_data)  # Append message data to the list
+        typ, data = my_mail.fetch(num, '(RFC822)')  # Extract message data
+        li.append(data)  # Append message data to the list
 
     return li
 
 
-def logic(msg_data):
+def logic(li):
+    lii=[]
     # Initialize the result dictionary for this email
-    formatted_result = {}
     html_converter = html2text.HTML2Text()
-    my_msg = email.message_from_bytes(msg_data)
+    for msg in li:
+        for response_part in msg:
+            if type(response_part) is tuple:
+                my_msg = email.message_from_bytes(response_part[1])
+                formatted_result = {}
+                if 'closed' in my_msg['subject']:
+                    formatted_result['Status'] = "closed"
+                if 'Hold' in my_msg['subject']:
+                    formatted_result['Status'] = 'Hold'
 
-    if 'closed' in my_msg['subject']:
-        formatted_result['Status'] = "closed"
-    if 'Hold' in my_msg['subject']:
-        formatted_result['Status'] = 'Hold'
+                if "Requisition #" in my_msg['subject']:
+                    value = my_msg['subject'].split("Requisition #")[1].strip('\n')
+                    value = value.strip().split('\n')[0].split()[0]
+                    formatted_result['Requisition ID'] = value
 
-    if "Requisition #" in my_msg['subject']:
-        value = my_msg['subject'].split("Requisition #")[1].strip('\n')
-        value = value.strip().split('\n')[0].split()[0]
-        formatted_result['Requisition ID'] = value
-
-    for part in my_msg.walk():
-        if part.get_content_type() == 'text/plain':
-            plain_text = part.get_payload()
-        if part.get_content_type() == 'text/html':
-            html_content = part.get_payload(decode=True).decode()
-            plain_text = html_converter.handle(html_content)
-            if "The following" in plain_text:
-                value = plain_text.split("The following")[1]
-                value = value.split('.')[0]
-                formatted_result["comment"] = value
-
-    return formatted_result
+                for part in my_msg.walk():
+                    if part.get_content_type() == 'text/plain':
+                        plain_text = part.get_payload()
+                    if part.get_content_type() == 'text/html':
+                        html_content = part.get_payload(decode=True).decode()
+                        plain_text = html_converter.handle(html_content)
+                        if "The following" in plain_text:
+                            value = plain_text.split("The following")[1]
+                            value = value.split('.')[0]
+                            formatted_result["comment"] = value
+                lii.append(formatted_result)
+    print(lii)
 
 
 email_data_list = Email('vinitha.s@vrdella.com', 'udbb zntf hhwe fbxl')
-for msg_data in email_data_list:
-    result = logic(msg_data)
-    print(result)
-
-
+logic(email_data_list)
+# result = logic(email_data_list)
+# print(result)
